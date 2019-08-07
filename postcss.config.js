b54832361e465da7f9d4c08135cfed9aa707eae7
Daniel Martinez <mikedmtz@gmail.com>
2019-08-07 08:42:09 -0400

    add nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const nested = require('postcss-nested');
 
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), autoprefixer()],
+  plugins: [tailwindcss('./tailwind.config.js'), nested(), autoprefixer()],
 };
