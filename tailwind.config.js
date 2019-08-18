6bdb4ca195600557eb7ea13bb4887df857561d69
Daniel Martinez <mikedmtz@gmail.com>
2019-08-18 16:32:41 -0400

    add breakpoints, refactor header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {},
+    screens: {
+      sm: { max: '640px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px' },
+    },
+    extend: {
+      colors: {
+        'my-orange': '#cb4b16',
+      },
+    },
   },
   variants: {},
   plugins: [],
